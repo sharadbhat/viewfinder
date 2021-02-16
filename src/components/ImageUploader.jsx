caffc2a635b34cc07fa01857893bd5087abf69d2
sharadbhat <sharad.mbhat@gmail.com>
2021-02-16 23:24:33 +0530

    image uploader

---
+++ src/components/ImageUploader.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { Upload, message } from 'antd';
+
+// Utils
+import { Context } from '../utils/Context'
+
+// Icons
+import { InboxOutlined } from '@ant-design/icons';
+
+const { Dragger } = Upload;
+
+class ImageUploader extends Component {
+  beforeUpload = async file => {
+    const reader = new FileReader()
+
+    reader.onloadend = e => {
+      this.context.setImageData(reader.result)
+      message.success('File uploaded successfully')
+    }
+    
+    reader.readAsArrayBuffer(file)
+
+    // Prevent upload
+    return false
+  }
+
+  render() {
+    return (
+      <div>
+        <Dragger
+          showUploadList={false}
+          multiple={false}
+          accept={['.jpg', '.jpeg']}
+          name='file'
+          beforeUpload={this.beforeUpload}
+        >
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">
+            Click or drag image to this area to upload
+          </p>
+        </Dragger>
+      </div>
+    )
+  }
+}
+
+ImageUploader.contextType = Context
+
+export default ImageUploader
