09b9c515c56b6eefdfe5865fbfe4292b0644ab1b
sharadbhat <sharad.mbhat@gmail.com>
2021-02-17 16:35:39 +0530

    sample list

---
+++ src/utils/sampleImagesList.js
@@ -0,0 +1,8 @@
+var samples = [
+  "https://raw.githubusercontent.com/sharadbhat/viewfinder/master/samples/sample1.jpg",
+  "https://raw.githubusercontent.com/sharadbhat/viewfinder/master/samples/sample2.jpg",
+  "https://raw.githubusercontent.com/sharadbhat/viewfinder/master/samples/sample3.jpg",
+  "https://raw.githubusercontent.com/sharadbhat/viewfinder/master/samples/sample4.jpg",
+]
+
+export default samples
